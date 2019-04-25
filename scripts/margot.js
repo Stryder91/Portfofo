d515693c9a4ece245cc23abbae9af6a1d8dc5747
Margot Rasamy <rasamymargot@gmail.com>
2019-04-25 13:58:40 +0200

    almost operational website

--- scripts/margot.js
+++ scripts/margot.js
@@ -2,18 +2,41 @@
 
 //get modal black element
 const modal = document.querySelector ('.modal_black')
-const message = document.querySelector ('.message')
+const mail = document.querySelector ('.mail_message')
+const number = document.querySelector ('.number_message')
 //get modal button
-const modalBtn = document.querySelector ('.button_mail')
+const mailBtn = document.querySelector ('.button_mail')
+const numberBtn = document.querySelector ('.button_call')
 //get close button
 const closeBtn = document.querySelector ('.closeButton')
+const closeBtn2 = document.querySelector ('.closeButton2')
 
 //click
-modalBtn.addEventListener ('click',
-function openModal (){
+mailBtn.addEventListener ('click',
+function openMail (){
   modal.classList.remove("closing")
-  message.classList.remove("closing")
+  mail.classList.remove("closing")
+  console.log('fre')
+})
+numberBtn.addEventListener ('click',
+function openNumber (){
+  modal.classList.remove("closing")
+  number.classList.remove("closing")
   console.log('fre')
 })
 
+closeBtn.addEventListener ('click',
+
+function () {modal.classList.add("closing")
+mail.classList.add("closing")
+}
+)
+
+closeBtn2.addEventListener ('click',
+function () {
+modal.classList.add("closing")
+number.classList.add("closing")
+
+}
+)
 //funciton
